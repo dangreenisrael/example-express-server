c23b5d95749c7ff0474bf9c0d1fe545b5471f082
Dan <dangreen.tlv@gmail.com>
2017-01-30 16:23:19 -0500

    Added cloud DB creds

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ const app = express();
 const bodyParser = require('body-parser');
 app.listen(9000);
 app.use(bodyParser.json());
-
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/OpenCharades');
+let mongoUrl = 'mongodb://localhost/OpenCharades';
+if (process.env.READ_ONLY_PASSWORD){
+  mongoUrl = `mongodb://read-only-user:${process.env.READ_ONLY_PASSWORD}@ds137759.mlab.com:37759/heroku_j8h75bxw`;
+}
+mongoose.connect(mongoUrl);
 mongoose.Promise = global.Promise;
 module.exports.mongoose = mongoose;
 const models = require('./models');
 
+app.get('/', (req, res)=>{
+  res.json({
+    GET:[
+      "/words/:categoryId",
+      "/categories"
+    ]
+  })
+});
 app.get('/words/:categoryId', (req, res)=> {
   const categoryId = req.params.categoryId;
   models.getWords(categoryId).then(words=>{
@@ -21,7 +32,6 @@ app.get('/words/:categoryId', (req, res)=> {
 
 app.get('/categories', (req, res)=> {
   models.getCategories().then(categories=>{
-    console.log(categories);
     res.json(categories);
   }, err=>{
     res.status(500).send(err)
